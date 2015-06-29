18fcd2178a78a45fe4270ed6bab333b8aab4d3e3
Pushkar Kumar Jain <pushkarjain1991@utexas.edu>
2015-06-29 14:47:29 -0500

    Updated connector2

--- examples/tsunami/PDAF-D_V1.11.1/testsuite/simplified_offline_geoclaw/python_script/connector2.py
+++ examples/tsunami/PDAF-D_V1.11.1/testsuite/simplified_offline_geoclaw/python_script/connector2.py
@@ -8,6 +8,7 @@ import sys
 import make_init
 import make_init_ens
 import pdaf_to_geoclaw
+import remove_file
 
 
 # First will try running using qinit.
@@ -33,36 +34,66 @@ def main():
     yupper = 50.e0
     ylower = -50.e0
     geoclaw_input = "hump.xyz"     
-    outfile_ens = "ens_"
+    radialbowl_files = ["bowl.topotype2", "Makefile", "setrun.py"]
+    radialbowl_path = "/h2/pkjain/Desktop/Pushkar/clawpack/geoclaw/examples/tsunami/bowl-radial/"
+    
+    #DA parameters
+    num_ens = 9
+    obs_t_interval = 10
+
     x = np.linspace(xlower,xupper,nxpoints)
     y = np.linspace(yupper,ylower,nypoints)
     xv,yv = np.meshgrid(x,y)
-    num_ens = 9
-    radialbowl_files = ["bowl.topotype2", "Makefile", "setrun.py"]
-    radialbowl_path = "/h2/pkjain/Desktop/Pushkar/clawpack/geoclaw/examples/tsunami/bowl-radial/"
-
         
     #Create main initial data file in format of the Geoclaw input
     mean_init_z = make_init.makeinit(xv, yv, geoclaw_input)
 
     #Create ensemble members based on the mean value vector
-    make_init_ens.makeinitens(mean_init_z,num_ens, outfile_ens)
+    make_init_ens.makeinitens(mean_init_z,num_ens, "ens_")
     
     #Convert format of ensemble to data input format of Geoclaw qinit
     firsttime = True
+
+
     for i in range(1, num_ens+1):
+        
+        #Check if path exists and create subfolders to run individual geoclaw
+        if os.access(str(i),os.F_OK):
+            remove_file.remove(str(i))
         os.mkdir(str(i), 0755)
+
+        #Change to subdirectory
         os.chdir(str(i))
+
+        #Define pdaf input and output file names
         pdaf_input = "../ens_" + str(i) + ".txt"
         pdaf_output = "../../ens_0"+str(i)+"_ana.txt"
+
+        #Prepare qinit files for geoclaw
         if firsttime:
             pdaf_to_geoclaw.pdaf_to_geoclaw(xv, yv, pdaf_input, geoclaw_input)
             #firsttime = False
         else:
             pdaf_to_geoclaw.pdaf_to_geoclaw(xv, yv, pdaf_output, geoclaw_input)
+
+        #Copy the radial bowl test case files to every sub directory
         for files in radialbowl_files:
             shutil.copy2(os.path.join(radialbowl_path,files),os.getcwd())
+
+        #Run Geoclaw forecast step
+        subprocess.call(["make",".output"])
+
+        #Extract water surface elevation from geoclaw fort.q file
+        eta = np.loadtxt("_output/fort.q00" + str(obs_t_interval), skiprows=9, usecols = [3])
+        np.savetxt("../ens_"+str(i)+".txt_new", eta)
+
+        
+        
+        #Go back one directory  
         os.chdir("../")
+        
+    #Run PDAF assimilation step
+
     # Run Geoclaw forecast step for all the ensemble members
 
 
